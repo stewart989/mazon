fe75ac27b1ca98552f8f4fc889302972deb069a9
stewart989 <131691081+stewart989@users.noreply.github.com>
2026-04-30 08:38:34 -0500

    Create  install-secure.sh

---
+++ install-secure.sh
@@ -0,0 +1,5 @@
+#!/bin/bash# 安全安装命令（推荐直接运行）
+curl -sL https://raw.githubusercontent.com/Gozargah/Marzban-scripts/master/marzban.sh -o /tmp/marzban.sh
+chmod +x /tmp/marzban.sh
+sudo mv /tmp/marzban.sh /usr/local/bin/marzban
+echo " Marzban 已安全安装"
